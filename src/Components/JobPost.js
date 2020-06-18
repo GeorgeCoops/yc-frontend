124f4131e0992a394ee2dc177d6f1b86143f44f6
GeorgeCoops <coopsgeorge@gmail.com>
2020-06-18 13:25:29 +0100

    GC - Spring additions to JobScroll compnonent

--- src/Components/JobPost.js
+++ src/Components/JobPost.js
@@ -1,18 +1,30 @@
 import React from "react";
 
 import styles from "../css/JobContainer.module.css";
+import { Spring } from "react-spring/renderprops";
 
 export default function JobPost(props) {
   const job = props.job;
 
   return (
-    <div className={styles.jobPost} onClick={() => props.clickedPost(job)}>
-      <hr className={styles.lineBreak}></hr>
-      <h2 className={styles.postPosition}>{job.position.toLowerCase()}</h2>
-      <h4 className={styles.postCompany}>{job.company.toUpperCase()}</h4>
-      <p className={styles.postDetails}>
-        {job.location} - {job.length}{" "}
-      </p>
-    </div>
+    <Spring
+      from={{ opacity: 0, marginTop: -500 }}
+      to={{ opacity: 1, marginTop: 0 }}
+    >
+      {(props) => (
+        <div
+          style={props}
+          className={styles.jobPost}
+          onClick={() => props.clickedPost(job)}
+        >
+          <hr className={styles.lineBreak}></hr>
+          <h2 className={styles.postPosition}>{job.position.toLowerCase()}</h2>
+          <h4 className={styles.postCompany}>{job.company.toUpperCase()}</h4>
+          <p className={styles.postDetails}>
+            {job.location} - {job.length}{" "}
+          </p>
+        </div>
+      )}
+    </Spring>
   );
 }
